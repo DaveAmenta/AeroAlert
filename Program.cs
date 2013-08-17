629e31873f7024834e7aa65ec1ed4777ffe0b8f0
Dave Amenta <dave@daveamenta.com>
2013-08-17 13:13:28 -0700

    Initial commit - old code import

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading;
+
+namespace AeroAlert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Help();
+                return;
+            }
+
+            int speed;
+            int.TryParse(args[0], out speed);
+            speed = Math.Max(speed, 1);
+
+            bool reset = !args.Contains("-k");
+            Color resetColor = aero.GetColor();
+
+            foreach (string a in args)
+            {
+                try
+                {
+                    Color c = Color.Empty;
+                    if (a.StartsWith("#"))
+                    {
+                        c = System.Drawing.ColorTranslator.FromHtml(a);
+                    }
+                    else if(a.Length > 2)
+                    {
+                        c = Color.FromName(a);
+                    }
+                    if (c != Color.Empty) aero.ChangeColor(c, speed);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex);
+                }
+            }
+            if (reset) aero.ChangeColor(resetColor, speed);
+        }
+
+        static void Help()
+        {
+            string str = "";
+            str += "\n" + ("AeroAlert - Dave Amenta - http://daveamenta.com");
+            str += "\n" + ("");
+            str += "\n" + ("-- Usage --");
+            str += "\n" + ("AeroAlert.exe <Step#> [-k] <Color1, [Color2], [Color3], ..., [ColorN]>");
+            str += "\n" + ("");
+            str += "\n" + ("Change the DWM Aero Color to the specified color(s), in the specified number of transform increments.");
+            str += "\n" + ("");
+            str += "\n" + ("-- Options --");
+            str += "\n" + ("Step#    Number of steps to complete the color transform.");
+            str += "\n" + ("-k       Persistent.  Do not set the previous color before exit.");
+            str += "\n" + ("");
+            str += "\n" + ("-- Color Format --");
+            str += "\n" + ("Known Color:     Blue, Green, Red, etc. (.NET System.Drawing.Color)");
+            str += "\n" + ("RGB Hex Color:   #RRGGBB");
+            str += "\n" + ("");
+            str += "\n" + ("-- Examples --");
+            str += "\n" + ("AeroAlert 100 Blue #ff00ee");
+            str += "\n" + ("     Change the color to Blue in 100 steps, then #ff00ee in 100 steps, then return to the user-defined color over 100 steps.\n");
+            str += "\n" + ("AeroAlert 1 -k Red");
+            str += "\n" + ("     Change the aero color to Red immediately.");
+            System.Windows.Forms.MessageBox.Show(str, "AeroAlert Help", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ aero.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AeroAlert
+{
+    public class aero
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        struct DwmColorParams
+        {
+            public uint color;
+            public uint param2;
+            public uint param3;
+            public uint param4;
+            public uint param5;
+            public uint param6;
+            public bool useTransparency;
+        };
+        [DllImport("dwmapi.dll", EntryPoint = "#131")]
+        static extern int DwmpSetColorizationParameters(ref DwmColorParams dcpParams, bool alwaysTrue);
+        [DllImport("dwmapi.dll", EntryPoint = "#127")]
+        static extern int DwmpGetColorizationParameters(out DwmColorParams dcpParams);
+
+        public static Color GetColor()
+        {
+            DwmColorParams p = new DwmColorParams();
+            DwmpGetColorizationParameters(out p);
+            return Color.FromArgb((int)p.color);
+        }
+
+        public static void ChangeColor(Color goalColor, int duration)
+        {
+            DwmColorParams p = new DwmColorParams();
+            DwmpGetColorizationParameters(out p);
+            Color startColor = Color.FromArgb((int)p.color);
+            if (goalColor == startColor) return;
+            CChanger ct = new CChanger(startColor, goalColor, duration);
+            while (ct.Transform())
+            {
+                Color intColor = ct.GetColor();
+                p.color = (uint)intColor.ToArgb();
+                p.param2 = p.color;
+
+                DwmpSetColorizationParameters(ref p, true);
+                Application.DoEvents();
+                Thread.Sleep(33);
+            }
+        }
+    }
+
+    class CChanger
+    {
+        public int currentStep;
+        private Color goal;
+        private Color start;
+        public int steps;
+
+        public CChanger(Color start, Color goal, int steps)
+        {
+            this.start = start;
+            this.goal = goal;
+            this.steps = steps;
+        }
+
+        public Color GetColor()
+        {
+            int red = this.start.R + ((this.currentStep * (this.goal.R - this.start.R)) / this.steps);
+            int green = this.start.G + ((this.currentStep * (this.goal.G - this.start.G)) / this.steps);
+            int blue = this.start.B + ((this.currentStep * (this.goal.B - this.start.B)) / this.steps);
+            int alpha = this.start.A + ((this.currentStep * (this.goal.A - this.start.A)) / this.steps);
+            return Color.FromArgb(alpha, red, green, blue);
+        }
+
+        public bool Transform()
+        {
+            if (this.currentStep < this.steps)
+            {
+                this.currentStep++;
+                return true;
+            }
+            return false;
+        }
+    }
+}
